e74744d6a2212aef782f188d5342b5a387b81249
Succ_Lord <cbnewton2000@gmail.com>
2019-09-12 15:42:09 -0400

    yee

--- Project1/Source.c
+++ Project1/Source.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 int main() {
 	char InputCharrot;
-	scanf_s("%c", &InputCharrot);
-	if ((65 <= InputCharrot) && (InputCharrot <= 90)) {
-		int Spaces;
-		int PrintCharrot = 64;
-		for (int CurrentCharrot = 65; CurrentCharrot <= InputCharrot; CurrentCharrot++) {
-			for (Spaces = InputCharrot - CurrentCharrot; Spaces > 0; Spaces--) {
-				printf(" ");
+	while (1) {
+		scanf_s("%c", &InputCharrot);
+		if ((65 <= InputCharrot) && (InputCharrot <= 90)) {
+			int Spaces;
+			int PrintCharrot = 64;
+			for (int CurrentCharrot = 65; CurrentCharrot <= InputCharrot; CurrentCharrot++) {
+				for (Spaces = InputCharrot - CurrentCharrot; Spaces > 0; Spaces--) {
+					printf(" ");
+				}
+				for (PrintCharrot++; PrintCharrot < CurrentCharrot; PrintCharrot++) {
+					printf("%c", PrintCharrot);
+				}
+				for (PrintCharrot; PrintCharrot > 64; PrintCharrot--) {
+					printf("%c", PrintCharrot);
+				}
+				printf("\n");
 			}
-			for (PrintCharrot++; PrintCharrot < CurrentCharrot; PrintCharrot++) {
-				printf("%c", PrintCharrot);
-			}
-			for (PrintCharrot; PrintCharrot > 64; PrintCharrot--) {
-				printf("%c", PrintCharrot);
-			}
-			printf("\n");
 		}
 	}
 	return 0;
